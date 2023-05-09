df2e5b3bc5b0fa3e32d627b3bae9624af74ac6a3
Luka Brkovic <coding@lbrkovic.com>
2023-05-09 07:58:10 +0200

    prisma tables

--- src/components/layout/SidebarTweetButton.tsx
+++ src/components/layout/SidebarTweetButton.tsx
@@ -1,19 +1,28 @@
-import Link from 'next/link';
-import { type FC } from 'react';
+import useLoginModal from '@/hooks/useLoginModal';
+import { useRouter } from 'next/router';
+import { useCallback, type FC } from 'react';
 import { FaFeather } from 'react-icons/fa';
 
 
 
 const SidebarTweetButton: FC = ({}) => {
+  const router = useRouter()
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+    
+  }, [loginModal])
+
   return (
-<Link href="/"> 
+<div onClick={onClick}> 
 <div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
     <FaFeather color="white" size="24" />
 </div>
 <div className='mt-6 hidden lg:flex justify-center px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
 <button className='hidden lg:block text-center font-semibold text-white text-xl'>Tweet</button>
 </div>
-</Link>
+</div>
 )
 }
 
